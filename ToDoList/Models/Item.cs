886ccdcfbd70ceabfcdc2e01e689657563edac37
Cristina Plesa <cristinaplesa8@gmail.com>
2021-05-04 21:19:57 -0700

    add static variable and code adding descriptions

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -4,10 +4,12 @@ namespace ToDoList.Models
   {
 
     public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> {};
 
     public Item(string description)
     {
       Description = description;
+      _instances.Add(this);
     }
 
   }
